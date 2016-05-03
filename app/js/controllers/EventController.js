c6a4e4f3c9405dd0da9fa4d3fc6091cf2412a65d
Eric Nshimiye - Intern <Eric.Nshimiye@bentley.com>
2016-05-03 16:40:07 -0400

    first commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,50 @@
+'use strict';
+
+eventsApp.controller('EventController', function($scope){
+    $scope.event = {
+        name : 'Angular Boot Camp',
+        date : '3/5/2016',
+        time : '10:30',
+        location :{
+            address : '863 Monseigneur Grandin',
+            city : 'Quebec',
+            province : 'Quebec, QC'
+        },
+        imageUrl : '/img/angularjs-logo.png',
+        sessions : [
+            {
+                name : 'Directives Masterclass',
+                creatorName : 'Bob Smith',
+                duration : '1 hr',
+                level : 'Advanced',
+                abstract : 'Vice semiotics echo park pinterest letterpress occupy, godard venmo aesthetic. Banh mi typewriter sriracha migas selvage blog. Viral slow-carb venmo, intelligentsia hammock microdosing health goth hoodie ethical selfies tumblr letterpress butcher distillery squid.',
+                upVoteCount : 0
+            },
+            {
+                name: 'Scopes for fun and profit',
+                creatorName : 'John Doe',
+                duration : '1 hr',
+                level : 'Introductory',
+                abstract : 'Vice semiotics echo park pinterest letterpress occupy, godard venmo aesthetic. Banh mi typewriter sriracha migas selvage blog. Viral slow-carb venmo, intelligentsia hammock microdosing health goth hoodie ethical selfies tumblr letterpress butcher distillery squid.',
+                upVoteCount : 0
+            },
+            {
+                name: 'Well behaved controllers',
+                creatorName : 'John Doe',
+                duration : '1 hr',
+                level : 'Introductory',
+                abstract : 'Vice semiotics echo park pinterest letterpress occupy, godard venmo aesthetic. Banh mi typewriter sriracha migas selvage blog. Viral slow-carb venmo, intelligentsia hammock microdosing health goth hoodie ethical selfies tumblr letterpress butcher distillery squid.',
+                upVoteCount : 0
+            }
+        ]
+    };
+    
+    $scope.upVoteSession = function(session){
+        session.upVoteCount++;
+    };
+    
+    $scope.downVoteSession = function(session){
+        session.upVoteCount--;
+    };
+    
+});
