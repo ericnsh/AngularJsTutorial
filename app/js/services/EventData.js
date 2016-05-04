2314eda583db9b7078e212d1c6c1050ef751595c
Eric Nshimiye - Intern <Eric.Nshimiye@bentley.com>
2016-05-04 13:19:50 -0400

    saving data with $resource

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,7 +1,12 @@
 eventsApp.factory('eventData',function($resource){
+    var resource = $resource('/data/event/:id', {id:'@id'});
     return {       
         getEvent : function(){
-             return $resource('/data/event/:id', {id:'@id'}).get({id:1});
+             return resource.get({id:1});
+        },
+        save : function(event){
+            event.id = 999; //Hardcoded ID : BAD AF
+            return resource.save(event);
         }
     };
 });
