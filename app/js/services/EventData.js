518f37dd7170333d93e70021a364973639e988d1
Eric Nshimiye - Intern <Eric.Nshimiye@bentley.com>
2016-05-04 10:44:38 -0400

    $http promises

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,14 +1,7 @@
-eventsApp.factory('eventData',function($http, $log){
-    return {
-        
-        getEvent : function(successCallback){
-             $http({method : 'GET',  url:'/data/event/1'})
-                .success(function(data, status, headers, config){
-                    successCallback(data);
-                })
-                .error(function(data, status, headers, config){
-                    $log.warn(data, status, headers, config);
-                });   
+eventsApp.factory('eventData',function($http){
+    return {       
+        getEvent : function(){
+             return $http({method : 'GET',  url:'/data/event/1'});
         }
     };
 });
