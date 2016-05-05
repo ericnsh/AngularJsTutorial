ae759addddd0b32ee8cd4e3a6b073997fdd32bdd
Eric Nshimiye - Intern <Eric.Nshimiye@bentley.com>
2016-05-05 12:01:45 -0400

    directive

--- app/js/app.js
+++ app/js/app.js
@@ -7,16 +7,25 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
                 templateUrl: 'templates/NewEvent.html',
                 controller : 'EditEventController'
             });
+            
         $routeProvider.when('/events',
             {
                 templateUrl: 'templates/EventList.html',
                 controller : 'EventListController'
             });
+            
         $routeProvider.when('/events/:eventId',
             {
                 templateUrl: 'templates/EventDetails.html',
                 controller : 'EventController'
             });    
+            
+        $routeProvider.when('/sampleDirective', 
+            {
+                templateUrl : 'templates/SampleDirective.html',
+                controller : 'SampleDirectiveController'                
+            });
+            
         $routeProvider.otherwise({redirectTo : '/events'});
         
         $locationProvider.html5Mode(true);
